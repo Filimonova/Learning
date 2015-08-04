61655cf4ce3e27237bc01a40423c9c3e45275b01
Filimonova <M.A.Filimonova@yandex.ru>
2015-08-05 00:03:59 +0300

    new methods

--- functions.php
+++ functions.php
@@ -1,164 +1,175 @@
 <?php
 require_once ("config.php");
+
 const QUERY_EXCEPTION_FILE = '\exception_query.txt';
 const METHOD_EXCEPTION_FILE = '\exception_method.txt';
 
-class MyQueryBuilderException extends Exception{
-	public function __construct($message, $code = 0, Exception $previous = null) {
-		parent::__construct($message, $code, $previous);
+class MyQueryBuilderException extends Exception
+{
+    public function __construct($message, $code = 0, Exception $previous = null) 
+	{
+		//код записи в файл
+	    parent::__construct($message, $code, $previous);
     }
-	public function __toString() {
+    public function __toString() 
+	{
         return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
-    }
+	}
 /**
 * file_exception_method запись исключений методов в файл
 */
-	public function file_exception_method(){
-		$file = __DIR__.METHOD_EXCEPTION_FILE;
-		file_put_contents($file,$this->getMessage()."\r\n" .'Code: '.$this->getCode()."\r\n"
-								."File: ".$this->getFile()."\r\n" ."Line: ".$this->getLine()."\r\n"."\r\n",FILE_APPEND);
+    public function fileExceptionMethod()
+	{
+	    $file = __DIR__.METHOD_EXCEPTION_FILE;
+	    file_put_contents($file,$this->getMessage()."\r\n" .'Code: '.$this->getCode()."\r\n"
+								."File: ".$this->getFile()."\r\n" ."Line: ".$this->getLine()
+								."\r\n"."\r\n",FILE_APPEND);
 	}
 /**
 * file_exception_method запись исключений запросов в файл
 */
-	public function file_exception_query(){
-		$file = __DIR__.QUERY_EXCEPTION_FILE;
-		file_put_contents($file,$this->getMessage()."\r\n" .'Code: '.$this->getCode()."\r\n"
-								."File: ".$this->getFile()."\r\n" ."Line: ".$this->getLine()."\r\n"."\r\n",FILE_APPEND);
-	}
-	
+    public function fileExceptionQuery()
+	{
+	    $file = __DIR__.QUERY_EXCEPTION_FILE;
+	    file_put_contents($file,$this->getMessage()."\r\n" .'Code: '.$this->getCode()."\r\n"
+								."File: ".$this->getFile()."\r\n" ."Line: ".$this->getLine()
+								."\r\n"."\r\n",FILE_APPEND);
+	}	
 }
-class MyQueryBuilder{
+
+class MyQueryBuilder
+{
 /**
 * $record строка формирования запроса
 * $val_array массив prepared statement 
-* $db дескриптор базы данных
-* $STH дескриптор состояния
+* $count_row количество строк затронутых в запросе
 */
-	private $record = '';
-	private $val_array = NULL;
-	public $db;
-	public $STH;
+    public $record = '';
+    private $val_array = NULL;
+	private $count_row = NULL;
+    public $db;
+    public $sth;
 /**
 *__construct cоздание экземпляра класса PDO и подключение к базе данных
 */	
-	public function __construct(){
-		$dsn = 'mysql:host='.HOST.';dbname='.DB_NAME;
+    public function __construct()
+	{ 
+	    $dsn = 'mysql:host='.HOST.';dbname='.DB_NAME;
 		$this->db = new PDO($dsn,USER,PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-		$this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	    $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	    $this->db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 	}
 /**
 *delete формирование строки запроса с ключевым словом DELETE 
 */		
-	public function delete (){
-		$this->record = 'DELETE ';
-		return $this;
+    public function delete ()
+	{
+	    $this->record = 'DELETE ';
+	    return $this;
 	}
 /**
 *insert формирование строки запроса с ключевым словом INSERT
 *@param $table string таблица для записи
 *@param $what поле(я) для записи 
 */
-	public function insert ($table,$what){
-		if (is_array($what)){
-			if (count ($what)>1){
-				$this->record = 'INSERT INTO ' .$table .' ' .'(' .implode (', ',$what) .')';
+    public function insert ($table,$what)
+	{
+	    if (is_array($what)){
+		    if (count ($what)>1){
+			    $this->record = 'INSERT INTO ' .$table .' ' .'(' .implode (', ',$what) .')';
 			}
+		} else{
+		    $this->record = 'INSERT INTO ' .$table .' ' .'(' .$what .')';
 		}
-		else{
-			$this->record = 'INSERT INTO ' .$table .' ' .'(' .$what .')';
-		}
-		return $this;
+	    return $this;
 	}
 /**
 *values формирование строки запроса с ключевым словом VALUES
 *@param $val значение(я) для записи
 *выбросит исключение без использования метода insert
 */
-	public function values ($val){
-		if (!strstr($this->record, 'INSERT')){
-			throw new MyQueryBuilderException ('Отсутствует INSERT');
-		}
-		else{
-			if (is_array($val)){
-				$this->val_array=$val;
-				$count = count ($val);
-				if ($count>1){
-					$str = '';
-						for ($i=0; $i<$count; $i++){
-							$str = $str .'?, ';
-						}
-					$str = substr($str, 0, -2);
-					$this->record = $this->record .' VALUES ' .'(' .$str .')';
-				}
+    public function values ($val)
+	{
+	    if (!strstr($this->record, 'INSERT')){
+		    throw new MyQueryBuilderException ('Отсутствует INSERT');
+		} else{
+		    if (is_array($val)){
+			    $this->val_array=$val;
+			    $count = count ($val);
+				    if ($count>1){
+					    $str = '';
+						    for ($i=0; $i<$count; $i++){
+							    $str = $str .'?, ';
+						    }
+					    $str = substr($str, 0, -2);
+					    $this->record = $this->record .' VALUES ' .'(' .$str .')';
+				    }
+			} else{
+			    $this->val_array[]=$val;
+			    $this->record = $this->record .' VALUES ' .'(' .'?' .')';
 			}
-			else{
-				$this->val_array[]=$val;
-				$this->record = $this->record .' VALUES ' .'(' .'?' .')';
-			}
-		return $this;
+	    return $this;
 		}
 	}
 /**
 *select формирование строки запроса с ключевым словом SELECT
 *@param $columns поля для выборки
 */
-	public function select($columns){
-		if (is_array($columns)){
-			if (count ($columns)>1){
+    public function select($columns)
+	{
+	    if (is_array($columns)){
+		    if (count ($columns)>1){
 				$this->record = 'SELECT ' . implode (', ',$columns);
 			}
+		} else{
+		    $this->record = 'SELECT ' .$columns;
 		}
-		else{
-			$this->record = 'SELECT ' .$columns;
-		}
-		return $this;
+	    return $this;
 	}
 /**
 *from формирование строки запроса с ключевым словом FROM
 *@param $table string таблица для выборки
 *выбросит исключение без использования метода select или delete 
 */
-	public function from($table){
-		if ((!strstr($this->record,'DELETE'))&&(!strstr($this->record,'SELECT'))){
-			throw new MyQueryBuilderException ('Отсутствует DELETE или SELECT');
+    public function from($table)
+	{
+	    if ((!strstr($this->record,'DELETE'))&&(!strstr($this->record,'SELECT'))){
+		    throw new MyQueryBuilderException ('Отсутствует DELETE или SELECT');
 		}
-		
-		$this->record = $this->record .' FROM ' .$table;
-		return $this;
-		
+	    $this->record = $this->record .' FROM ' .$table;
+	    return $this;	
 	}
 /**
 *update формирование строки запроса с ключевым словом update
 *@param $table таблица для записи
 */
-	public function update($table){
-		$this->record = 'UPDATE ' .$table;
-		return $this;
+    public function update($table)
+	{
+	    $this->record = 'UPDATE ' .$table;
+	    return $this;
 	}
 /**
 *set формирование строки запроса с ключевым словом SET
 *@param $what array: $key имя поля, $value значение для записи
 *выбросит исключение без использования метода update
 */	
-	public function set($what){
-		if (!strstr($this->record, 'UPDATE')){
-			throw new MyQueryBuilderException ('Отсутствует UPDATE');
-		}
-		else{
-			$this->record = $this->record . ' SET ';
-			$this->val_array=array_values($what);
-				if (count($what)>1){
-					foreach ($what as $key=>$value){
-						$this->record = $this->record .$key .' = ' .'?'.', ';
+    public function set($what)
+	{
+	    if (!strstr($this->record, 'UPDATE')){
+		    throw new MyQueryBuilderException ('Отсутствует UPDATE');
+		} else{
+		    $this->record = $this->record . ' SET ';
+		    $this->val_array=array_values($what);
+			    if (count($what)>1){
+				    foreach ($what as $key=>$value){
+					    $this->record = $this->record .$key .' = ' .'?'.', ';
 					}
-					$this->record = substr($this->record, 0, -2);
+				    $this->record = substr($this->record, 0, -2);
+				} else{
+				    $this->record = $this->record . ' SET ' .$what .' = ' .'?' ;
 				}
-				else{
-					$this->record = $this->record . ' SET ' .$what .' = ' .'?' ;
-				}
-			return $this;
-		}	
+		}
+        return $this;		
 	}
 /**
 *what формирование строки запроса с ключевым словом WHAT
@@ -168,18 +179,32 @@ class MyQueryBuilder{
 *$b array "белый список" операторов сравнения
 *выбросит исключение если при записи строка запроса будет пустой
 */	
-	public function where($what,$oper,$val){
-		if (!$this->record){
-			throw new MyQueryBuilderException ('Невозможно записать WHERE');
+    public function where($what,$oper,$val)
+	{
+	    if (empty($this->record)){
+		    throw new MyQueryBuilderException ('Невозможно записать WHERE');
+		}else{
+		    $b = array ("=", ">", "<", ">=", "<=", "<>", "LIKE", "IN", "NOT IN", "BETWEEN", "IS NULL");
+		    $key = array_search ($oper, $b);
+		    $oper = $b[$key];
+			    if (($oper == 'IN')||($oper == 'NOT IN')){
+				    $this->val_array=array_values($val);
+				    $this->record = $this->record .' WHERE '.$what .' ' .$oper .' ' .'(';
+					    foreach ($val as $value){
+						    $this->record = $this->record .'?'.', ';
+						}
+				    $this->record = substr($this->record, 0, -2).')';
+				} elseif($oper == 'BETWEEN'){
+				    $this->val_array=array_values($val);
+				    $this->record = $this->record .' WHERE '.$what .' ' .$oper .' ' .'?' .' ' .'AND' .' ' .'?';
+				} elseif($oper == "IS NULL"){
+				    $this->record = $this->record .' WHERE '.$what .' ' .$oper;
+				} else{
+				    $this->val_array[]= $val;
+				    $this->record = $this->record .' WHERE ' .$what .' '.$oper.' '.'?';
+				}	
 		}
-		else{
-			$b = array ("=", ">", "<", ">=", "<=", "<>", "LIKE");
-			$key = array_search ($oper, $b);
-			$oper = $b[$key];
-			$this->val_array[]= $val;
-			$this->record = $this->record .' WHERE ' .$what .' '.$oper.' '.'?';
-			return $this;	
-		}
+	    return $this;
 	}
 /**
 *and_where формирование строки запроса с использованием ключевого слова WHERE и оператора AND
@@ -189,18 +214,32 @@ class MyQueryBuilder{
 *$b array "белый список" операторов сравнения
 *выбросит исключение без использования метода where
 */
-	public function and_where($what,$oper,$val){
-		if (!strstr ($this->record, 'WHERE')){
-			throw new MyQueryBuilderException ('Отсутствует WHERE');
+    public function andWhere($what,$oper,$val)
+	{
+	    if (!strstr ($this->record, 'WHERE')){
+		    throw new MyQueryBuilderException ('Отсутствует WHERE');
+		} else{
+		    $b = array ("=", ">", "<", ">=", "<=", "<>", "LIKE", "IN", "NOT IN", "BETWEEN", "IS NULL");
+		    $key = array_search ($oper, $b);
+		    $oper = $b[$key];
+			    if (($oper == 'IN')||($oper == 'NOT IN')){
+				    $this->val_array=array_values($val);
+				    $this->record = $this->record .' AND '.$what .' ' .$oper .' ' .'(';
+					    foreach ($val as $value){
+						    $this->record = $this->record .'?'.', ';
+						}
+				    $this->record = substr($this->record, 0, -2).')';
+				} elseif($oper == 'BETWEEN'){
+					$this->val_array=array_values($val);
+					$this->record = $this->record .' AND '.$what .' ' .$oper .' ' .'?' .' ' .'AND' .' ' .'?';
+				} elseif($oper == "IS NULL"){
+			        $this->record = $this->record .' AND '.$what .' ' .$oper;
+				} else{
+				    $this->val_array[]= $val;
+				    $this->record = $this->record .' AND ' .$what .' '.$oper.' '.'?';
+				}
 		}
-		else{
-			$b = array ("=", ">", "<", ">=", "<=", "<>", "LIKE");
-			$key = array_search ($oper, $b);
-			$oper = $b[$key];
-			$this->val_array[]= $val;
-			$this->record = $this->record .' AND ' .$what .' '.$oper.' '.'?';
-			return $this;
-		}
+	    return $this;
 	}
 /**
 *or_where формирование строки запроса с использованием ключевого слова WHERE и оператора OR
@@ -210,48 +249,64 @@ class MyQueryBuilder{
 *$b array "белый список" операторов сравнения
 *выбросит исключение без использования метода where
 */
-	public function or_where($what,$oper,$val){
-		if (!strstr ($this->record, 'WHERE')){
-			throw new MyQueryBuilderException ('Отсутствует WHERE');
+    public function orWhere($what,$oper,$val)
+	{
+	    if (!strstr ($this->record, 'WHERE')){
+		    throw new MyQueryBuilderException ('Отсутствует WHERE');
+		} else{
+		    $b = array ("=", ">", "<", ">=", "<=", "<>", "LIKE", "IN", "NOT IN", "BETWEEN", "IS NULL");
+		    $key = array_search ($oper, $b);
+		    $oper = $b[$key];
+			    if (($oper == 'IN')||($oper == 'NOT IN')){
+				    $this->val_array=array_values($val);
+				    $this->record = $this->record .' OR '.$what .' ' .$oper .' ' .'(';
+					    foreach ($val as $value){
+						    $this->record = $this->record .'?'.', ';
+						}
+				    $this->record = substr($this->record, 0, -2).')';
+				} elseif($oper == 'BETWEEN'){
+				    $this->val_array=array_values($val);
+				    $this->record = $this->record .' OR '.$what .' ' .$oper .' ' .'?' .' ' .'AND' .' ' .'?';
+				} elseif($oper == "IS NULL"){
+				    $this->record = $this->record .' OR '.$what .' ' .$oper;
+				} else{
+				    $this->val_array[]= $val;
+				    $this->record = $this->record .' OR ' .$what .' '.$oper.' '.'?';
+				}
 		}
-		else{
-			$b = array ("=", ">", "<", ">=", "<=", "<>", "LIKE");
-			$key = array_search ($oper, $b);
-			$oper = $b[$key];
-			$this->val_array[]= $val;
-			$this->record = $this->record .' OR ' .$what .' '.$oper.' '.'?';
-			return $this;
-		}
+	    return $this;
 	}
 /**
 *limit формирование строки запроса с ключевым словом LIMIT
 *@param limit int лимит запроса
 *выбросит исключение если при записи строка запроса будет пустой 
 */
-	public function limit($limit){
-		if (!$this->record){
-			throw new MyQueryBuilderException ('Невозможно записать LIMIT');
+    public function limit($limit)
+	{
+	    if (empty($this->record)){
+		    throw new MyQueryBuilderException ('Невозможно записать LIMIT');
+		} else{
+		    $limit = intval($limit);
+		    $this->val_array[]= $limit;
+		    $this->record = $this->record .' LIMIT ' .'?';
 		}
-		else{
-			$limit = intval($limit);
-			$this->record = $this->record .' LIMIT ' .$limit;
-			return $this;
-		}
+	    return $this;
 	}
 /**
 *offset формирование строки запроса с ключевым словом OFFSET
 *@param offset int значение смещения
 *выбросит исключение без использования метода limit
 */
-	public function offset ($offset){
-		if (!strstr($this->record, 'LIMIT')){
-			throw new MyQueryBuilderException ('Отсутствует LIMIT');
+    public function offset ($offset)
+	{
+	    if (!strstr($this->record, 'LIMIT')){
+		    throw new MyQueryBuilderException ('Отсутствует LIMIT');
+		} else{
+		    $offset = intval ($offset);
+		    $this->val_array[]= $offset;
+		    $this->record = $this->record .' OFFSET ' .'?';
 		}
-		else{
-			$offset = intval ($offset);
-			$this->record = $this->record .' OFFSET ' .$offset;
-			return $this;
-		}
+	    return $this;
 	}
 /**
 *order формирование строки запроса с ключевым словом ORDER
@@ -259,42 +314,92 @@ class MyQueryBuilder{
 *@param $por порядок сортировки, если не указан то по умолчанию ASC 
 *$orders array "белый список" значений сортировки
 */
-	public function order ($columns, $por){
-		if (!$this->record){
-			throw new MyQueryBuilderException ('Невозможно записать ORDER');
-		}
-		else{
-			$orders = array("ASC","DESC");
-			$key = array_search ($por, $orders);
-			$orderby = $orders[$key];
-				if (is_array($columns)){
-					if (count ($columns)>1){
-						$this->record = $this->record .' ORDER BY ' . implode (', ',$columns) .' ' .$orderby;
-					}
+    public function order ($columns, $por)
+	{
+	    if (empty($this->record)){
+		    throw new MyQueryBuilderException ('Невозможно записать ORDER');
+		} else{
+		    $orders = array("ASC","DESC");
+		    $key = array_search ($por, $orders);
+		    $orderby = $orders[$key];
+			    if (is_array($columns)){
+				    if (count ($columns)>1){
+					    $this->record = $this->record .' ORDER BY ' . implode (', ',$columns) .' ' .$orderby;
+				    }
+				} else{
+				    $this->record = $this->record .' ORDER BY ' .$columns .' ' .$orderby;
 				}
-				else{
-					$this->record = $this->record .' ORDER BY ' .$columns .' ' .$orderby;
-				}
-			return $this;
 		}
+	    return $this;
+	}
+	
+	public function selectJoin($what, $join, $val, $oper)
+	{
+		$joining = array ("INNER", "LEFT", "RIGHT", "FULL", "CROSS");
+		    $key = array_search ($join, $joining);
+		    $join = $joining[$key];
+		$operators = array ("=", ">", "<", ">=", "<=", "<>");
+		    $key = array_search ($oper, $operators);
+		    $oper = $operators[$key];
+		$this->record = 'SELECT ';
+		    foreach ($what as $key=>$value){
+		        $this->record = $this->record .$key .'.' .$value .', ';
+		    }
+		$this->record = substr($this->record, 0, -2).' FROM ';
+		    foreach ($what as $key=>$value){
+				$tmp_line = $tmp_line .$key .' ' .'!';
+		    }
+		$tmp_line = substr($tmp_line, 0, -2);
+		$tmp_line = str_replace('!', $join .' JOIN ', $tmp_line);	
+		$this->record = $this->record .$tmp_line .' ON ';
+		$this->val_array=array_values($val);
+		    foreach ($val as $key=>$value){
+		        $this->record = $this->record .$key .'.' .'?' .' ' .$oper .' ';
+		    }
+		$this->record = substr($this->record, 0, -2);
+		echo $this->record;
 	}
 /**
-*query выполнение запроса 
+*query выполнение запроса с подготовленными выражениями
 */	
-	public function query(){
-		
-		$this->STH = $this->db->prepare($this->record);
-		$this->STH->execute($this->val_array);
-		
-		return $this;
+    public function query()
+	{
+	    $this->sth = $this->db->prepare($this->record);
+	    $this->sth->execute($this->val_array);
+	    return $this;
 	}
 /**
-*getResult получение результатов запроса 
+*getArray получение результа запроса в виде массива
 */	
-	public function getResult(){
-		$result = $this->STH->fetchAll();
-		print_r($result);
+    public function getArray()
+	{
+	    $result = $this->sth->fetchAll();
+	    print_r($result);
 	}
-		
+/**
+*getRow получение результа запроса в виде количества затронутых строк
+*/	
+	public function getRow()
+	{
+		if (isset($this->count_row)){
+			$count = $this->count_row;
+		} else{
+			$count = $this->sth->rowCount();
+		}
+		print('Количество затронутых строк: '. $count);
+	}
+/**
+*rawQuery выполнение запроса, построенного самостоятельно
+*@param $line_sql содержит строку запроса
+*/	
+    public function rawQuery($line_sql)
+	{
+	    if (strstr($line_sql,'SELECT')){
+		    $this->sth = $this->db->query($line_sql);
+		} else{
+			$this->count_row = $this->db->exec($line_sql);
+        }
+	    return $this;
+	}
 }
 ?>
--- index.php
+++ index.php
@@ -2,16 +2,29 @@
 require_once ("functions.php");
 
 $obj = new MyQueryBuilder;
-	
-	try{ 
-	$obj->select('name')->from('users')->where('id','=',4);
-	 }
-	catch (MyQueryBuilderException $m){
-		echo 'Возникла ошибка при формировании строки запроса',$m->file_exception_method();
+$a = array("age"=>51,
+           "city"=>"Самара",
+           "pol"=>"женский");
+    try{ 
+        //$obj->update('users')->set($a)->where('id','=','22');
 	}
-	catch (Exception $e){
-		echo 'Возникла ошибка другого типа',  $e->getMessage();
+    catch (MyQueryBuilderException $m){
+	    echo 'Возникло исключение при формировании строки запроса',$m->fileExceptionMethod();
+	}
+    catch (Exception $e){
+	    echo 'Возникло исключение другого типа',  $e->getMessage();
 	} 
-	$obj->query()->getResult();
+	//$obj->query()->getRow();	
 	
+/* $sql_upd = 'UPDATE users SET age = 27 WHERE id = 22';
+$sql_sel = "SELECT name FROM users WHERE pol = 'женский'";
+$obj->rawQuery($sql_upd)->getRow(); */
+
+$what1 = array ("users"=>"name",
+               "contacts"=>"phone");
+$join1 = "INNER";
+$val1 = array ("users"=>"id",
+              "contacts"=>"id_user");
+$obj->selectJoin($what1, $join1, $val1, '>');
+$obj->query()->getArray();
 ?>
